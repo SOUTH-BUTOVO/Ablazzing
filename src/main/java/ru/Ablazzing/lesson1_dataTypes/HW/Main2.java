ec00f4809721d191da40d36cc18724f698c9da92
SerGit <sergey-net.com86@mail.ru>
2024-04-22 17:11:29 +0300

    lesson 1, HW 8.3/8
    Signed-off-by: SerGit <sergey-net.com86@mail.ru>

---
+++ src/main/java/ru/Ablazzing/lesson1_dataTypes/HW/Main2.java
@@ -0,0 +1,9 @@
+package ru.Ablazzing.lesson1_dataTypes.HW;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Тест текст!");
+        System.out.println("Тест текст2!");
+        System.out.println("Тест текст3!");
+    }
+}
